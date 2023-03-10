0d534e56b06a9ef201e8b6de4d61979db78d12e5
Jason Hargrove <rijaha@gmail.com>
2023-03-10 15:55:31 -0600

    able to compile files

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+"use strict";
+// simple function
+const createMessage = (name) => {
+    return 'Hello, ' + name;
+};
+let result = createMessage('Bob');
+console.log(result);
---
+++ js/variables.js
@@ -0,0 +1,8 @@
+"use strict";
+// TypeScript Variables
+let fistName = 'Jason';
+let age = 47;
+let activeMember = true;
+console.log(typeof age);
+let someVar;
+// firstName = 123
