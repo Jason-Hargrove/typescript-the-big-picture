2d902df9a78fc5e3e8a95ad5546347d27eee6d99
Jason Hargrove <rijaha@gmail.com>
2023-03-09 21:30:25 -0600

    generated js

--- basic.ts
+++ basic.ts
@@ -9,3 +9,11 @@ class Artist {
     return 'Hello, my name is ' + this.name
   }
 }
+
+// create a new instance
+let firstcustomer = new Artist('Bruce')
+let newMessage: string = firstcustomer.announce()
+
+// change the text on the page
+let webHeading = document.querySelector('h1')
+webHeading!.textContent = newMessage
